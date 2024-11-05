0389088336f4254d466846eafec28a2e6825c4a9
João Vitor Escudeiro <joaovitor.escudeiro01@gmail.com>
2024-11-04 21:17:12 -0300

    Adicionando projpostget

--- servidor.js
+++ servidor.js
@@ -1,15 +1,57 @@
-require('colors');
-
 let http = require('http');
 let express = require('express');
+var colors = require('colors');
+let bodyParser = require("body-parser");
 
 var app = express();
-
+app.use(express.static('./public/', { index: 'projects.html' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', './views');
 // app.use(express.static('./public'));
-app.use(express.static('./public/', { index: 'home.html' }))
 
 var server = http.createServer(app);
-
 server.listen(80);
 
 console.log('Servidor rodando...'.rainbow);
+
+
+app.get('/cadastra', function (requisicao, resposta) {
+    resposta.redirect('projpostget/Cadastro.html')
+})
+
+app.get('/login', function (requisicao, resposta) {
+    resposta.redirect('projpostget/Login.html')
+})
+
+app.get('/inicio', function (requisicao, resposta) {
+    var nome = requisicao.query.info;
+    console.log(nome);
+    resposta.redirect('projects.html')
+})
+
+// app.post('/inicio', function (requisicao, resposta) {
+//     var data = requisicao.body.data;
+//     console.log(data);
+// })
+
+// usa requisicao.body.nome; no post e requisicao.query.nome; no get
+
+
+app.post('/cadastro', function (requisicao, resposta) {
+    var nome = requisicao.body.nome;
+    var sobrenome = requisicao.body.sobrenome;
+    var nascimento = requisicao.body.nascimento;
+    var usuario = requisicao.body.usuario;
+    var senha = requisicao.body.senha;
+
+    resposta.render('resposta', { nome, sobrenome, nascimento, usuario, senha })
+})
+
+app.post('/logar', function (requisicao, resposta) {
+    var usuario = requisicao.body.usuario;
+    var senha = requisicao.body.senha;
+
+    resposta.render('resposta', { usuario, senha })
+})
