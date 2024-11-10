f4b2e56d67a3ea5c1eb08d36d4ffdcc6d9b6e01d
João Vitor Escudeiro <joaovitor.escudeiro01@gmail.com>
2024-11-10 17:52:57 -0300

    Adicionando projeto Blog

--- servidor.js
+++ servidor.js
@@ -2,6 +2,11 @@ let http = require('http');
 let express = require('express');
 var colors = require('colors');
 let bodyParser = require("body-parser");
+let mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb+srv://joao:D1Kk2HFjgICw0NjC@cluster0.ndksp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 var app = express();
 app.use(express.static('./public/', { index: 'projects.html' }));
@@ -39,19 +44,100 @@ app.get('/inicio', function (requisicao, resposta) {
 // usa requisicao.body.nome; no post e requisicao.query.nome; no get
 
 
-app.post('/cadastro', function (requisicao, resposta) {
-    var nome = requisicao.body.nome;
-    var sobrenome = requisicao.body.sobrenome;
-    var nascimento = requisicao.body.nascimento;
-    var usuario = requisicao.body.usuario;
-    var senha = requisicao.body.senha;
+// app.post('/cadastro', function (requisicao, resposta) {
+//     var nome = requisicao.body.nome;
+//     var sobrenome = requisicao.body.sobrenome;
+//     var nascimento = requisicao.body.nascimento;
+//     var usuario = requisicao.body.usuario;
+//     var senha = requisicao.body.senha;
 
-    resposta.render('resposta', { nome, sobrenome, nascimento, usuario, senha })
+//     resposta.render('resposta', { nome, sobrenome, nascimento, usuario, senha })
+// })
+
+app.post("/cadastro", function (req, resp) {
+    // salva dados no banco
+    client.db("exemplo_bd").collection("usuarios").insertOne(
+        {
+            db_nome: req.body.nome,
+            db_login: req.body.usuario,
+            db_senha: req.body.senha
+        }, function (err) {
+            if (err) {
+                resp.render('resposta_erro_cadastro', { resposta: "Erro ao cadastrar usuário!" })
+            } else {
+                var usuario = req.body.usuario;
+                var senha = req.body.senha;
+                resp.render('resposta', { usuario, senha })
+            };
+        });
+});
+
+// app.post('/logar', function (requisicao, resposta) {
+//     var usuario = requisicao.body.usuario;
+//     var senha = requisicao.body.senha;
+
+//     resposta.render('resposta', { usuario, senha })
+// })
+
+app.post("/logar", function (req, resp) {
+    // busca um usuário no banco de dados
+    client.db("exemplo_bd").collection("usuarios").find(
+        {
+            db_login: req.body.usuario,
+            db_senha: req.body.senha
+        }).toArray(function (err, items) {
+            console.log(items);
+            if (items.length == 0) {
+                resp.render('resposta_erro_login', { resposta: "Usuário/senha não encontrado!" })
+            } else if (err) {
+                resp.render('resposta_erro_login', { resposta: "Erro ao logar usuário!" })
+            } else {
+                var usuario = req.body.usuario;
+                var senha = req.body.senha;
+                resp.render('resposta', { usuario, senha })
+            };
+        });
+});
+
+// Funcoes Exemplo bd
+
+app.get('/cadastrar_livro', function (req, res) {
+    let nome = req.query.cadastra_nome
+    let autor = req.query.cadastra_autor
+    let isbn = req.query.cadastra_isbn
+    let editora = req.query.cadastra_editora
+    let data = req.query.cadastra_data
+
+    res.render('respostabd', { nome, autor, isbn, editora, data })
 })
 
-app.post('/logar', function (requisicao, resposta) {
-    var usuario = requisicao.body.usuario;
-    var senha = requisicao.body.senha;
 
-    resposta.render('resposta', { usuario, senha })
+app.get('/blog', async function (requisicao, resposta) {
+    try {
+        const posts = await client.db("blog_db").collection("posts").find().toArray();
+        resposta.render('blog', { posts });
+    } catch (err) {
+        console.error('Erro ao buscar posts:', err);
+        resposta.render('resposta_erro_cadastro', { resposta: "Erro ao carregar posts!" });
+    }
+});
+
+app.get('/criar', function (requisicao, resposta) {
+    resposta.render('criarpost')
 })
+
+
+app.post("/criarpost", async function (req, resp) {
+    try {
+        await client.db("blog_db").collection("posts").insertOne({
+            titulo: req.body.titulo,
+            resumo: req.body.resumo,
+            conteudo: req.body.conteudo
+        });
+        resp.redirect('/blog'); // Redirecionar para ver todos os posts
+    } catch (err) {
+        console.error('Erro ao criar post:', err);
+        resp.render('resposta_erro_cadastro', { resposta: "Erro ao criar post!" });
+    }
+});
+
